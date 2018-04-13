186f70ab89cd9db904340a58b5ecbe836125130f
Travis Roach <travis.roach24@gmail.com>
2018-04-13 11:51:28 -0600

    added play sound function

--- app.js
+++ app.js
@@ -2,3 +2,15 @@ var hula = document.querySelector("#hula")
 
 hula.classList.add("bounceInLeft")
 console.log(hula.style)
+
+
+// // play sound template function
+// function playSound(e) {
+//     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+//     const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+//     if (!audio) return;
+
+//     key.classList.add('playing');
+//     audio.currentTime = 0;
+//     audio.play();
+// }
